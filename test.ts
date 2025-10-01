a7bd4da8b026f65f5f4c480f6756fbd8122db084
Itti Srisumalai <i2ktel@hotmail.com>
2025-10-02 01:49:22 +0700

    fix test.ts

--- test.ts
+++ test.ts
@@ -1,12 +1,12 @@
 // tests go here; this will not be compiled when this package is used as a library
-cayenneLPP.register_digital(LPP_Direction.Input_Port, DigitalPin.P0)
-cayenneLPP.register_analog(LPP_Direction.Input_Port, AnalogPin.P1)
-cayenneLPP.register_sensor(LPP_Bit_Sensor.Light)
-cayenneLPP.register_sensor(LPP_Bit_Sensor.LED_Brightness)
-cayenneLPP.register_sensor(LPP_Bit_Sensor.Temperature)
+cayenneLPP.add_digital(LPP_Direction.Input_Port, DigitalPin.P0)
+cayenneLPP.add_analog(LPP_Direction.Input_Port, AnalogPin.P1)
+cayenneLPP.add_sensor(LPP_Bit_Sensor.Light)
+cayenneLPP.add_sensor(LPP_Bit_Sensor.LED_Brightness)
+cayenneLPP.add_sensor(LPP_Bit_Sensor.Temperature)
 basic.forever(function () {
     basic.showString("" + cayenneLPP.lpp_upload() + cayenneLPP.lpp(
-    LPP_DATA_TYPE.Temperature_Sensor,
+    LPP_DATA_TYPE.Temperature,
     99,
     25
     ))
